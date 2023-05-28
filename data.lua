859ca733c2483f0d041e8700e3304b26ee772962
PlateGlassArmour <ikoene@yahoo.ca>
2023-05-28 12:10:09 -0400

    Update and name change

---
+++ data.lua
@@ -0,0 +1,5 @@
+require("prototypes.item")
+require("prototypes.entity")
+require("prototypes.recipe")
+require("prototypes.technology")
+data.raw["roboport"]["roboport"]["fast_replaceable_group"] = "roboport"
